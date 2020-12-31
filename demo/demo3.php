10ee1794bef402964a20cd36450320490b62ddf6
Stefan Moises <beffy@proudcommerce.com>
2020-12-31 09:23:21 +0100

    Add image to example

--- demo/demo3.php
+++ demo/demo3.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Demo drawing #2
  */
@@ -11,13 +12,18 @@ use adamasantares\dxf\Creator;
 use adamasantares\dxf\Color;
 use adamasantares\dxf\LineType;
 
+//setting image data
+$path = "./peludito.png";
+$size = getimagesize($path);
+$width = $size[0];
+$height = $size[1];
+
 (new Creator())
     ->setColor(Color::rgb(0, 100, 0))
     ->addEllipse(-20, 0, 0, -20, 30, 0, 0.5)
     ->setLayer('2', Color::MAGENTA, LineType::SOLID)
     ->addEllipseBy3Points(20, 0, 0, 20, 30, 0, 35, 0, 0)
+    ->addImage(0, 0, 0, 50, 50, 0, $path, $width, $height)
     ->saveToFile(dirname(__FILE__) . '/demo3.dxf');
 
 exit("   Done (" . dirname(__FILE__) . "/demo3.dxf)\n");
-
-
